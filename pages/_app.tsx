636b3d70d1c7de750265201576749a0c1bcedfea
awaisspk <iamawaisiqball@gmail.com>
2021-11-04 02:16:19 +0500

    Refactor : Changes in _app and _document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app';
+import {globalStyles} from '../styles/globalStyles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  globalStyles();
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;

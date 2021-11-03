61e4be14caa951be9d7a0ff94eca4ddec476bf62
awaisspk <iamawaisiqball@gmail.com>
2021-11-04 02:16:43 +0500

    Day1 project complete'

---
+++ src/Week/day1/index.tsx
@@ -0,0 +1,115 @@
+import {styled} from '@stitchesConfig';
+import {Box} from '@components/Box';
+import {Flex} from '@components/Flex';
+import {Text} from '@components/Text';
+import {ThumbUp} from '@components/Box/Icons/ThumbUp';
+import {ThumbDown} from '@components/Box/Icons/ThumbDown';
+
+export const Day1 = () => {
+  return (
+    <Container flow="col" main="spaceEvenly" cross="center">
+      <Box css={{bc: 'white', px: '$12', py: '$12', br: '25px'}}>
+        <Flex cross="center" main="spaceBtw" css={{maxW: '$3'}}>
+          <Text
+            css={{
+              color: '#1F205D',
+              fontSize: '22px',
+              fontWeight: '$700',
+              flexBasis: '55%',
+            }}
+          >
+            What do you think of the article
+          </Text>
+          <Flex gap="1">
+            <Flex
+              center
+              css={{
+                border: '2px solid #ECECF9',
+                br: '$lg',
+                px: '$4',
+                py: '$4',
+                textAlign: 'center',
+                transition: '300ms',
+              }}
+            >
+              <ThumbUp />
+            </Flex>
+            <Flex
+              center
+              css={{
+                border: '2px solid #ECECF9',
+                br: '$lg',
+                px: '$4',
+                py: '$4',
+              }}
+            >
+              <ThumbDown />
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+
+      <Box css={{bc: 'white', px: '$12', py: '$12', br: '25px'}}>
+        <Text
+          css={{
+            color: '#1F205D',
+            fontSize: '22px',
+            fontWeight: '$700',
+            flexBasis: '55%',
+          }}
+        >
+          Tags
+        </Text>
+        <Flex wrap="wrap" gap="2" css={{maxW: '$6'}}>
+          <Pill>housing benefits</Pill>
+          <Pill>housing benefits</Pill>
+          <Pill>One-time benefit</Pill>
+          <Pill>Parental benefits</Pill>
+          <Pill>parental scholarship</Pill>
+          <Pill>social assitance</Pill>
+          <Pill>benefit</Pill>
+          <Pill>social assitance</Pill>
+        </Flex>
+      </Box>
+    </Container>
+  );
+};
+
+const StyledPill = styled('div', {
+  px: '$2',
+  py: '$2',
+  border: '2px solid #ECECF9',
+  br: '$lg',
+  userSelect: 'none',
+  transition: '200ms',
+
+  $$textColor: '#151757',
+
+  '@hover': {
+    '&:hover': {
+      $$textColor: 'white',
+      backgroundColor: '#151757',
+    },
+  },
+
+  [`& ${Text}`]: {
+    color: '$$textColor',
+    lineHeight: '$0',
+    fontSize: '$1',
+  },
+});
+
+const Pill: React.FC = ({children}) => {
+  return (
+    <StyledPill>
+      <Text>{children}</Text>
+    </StyledPill>
+  );
+};
+
+const Container = styled(Flex, {
+  width: '100%',
+  minHeight: '100vh',
+  background: '#EBEBFB',
+  boxShadow: '0px 30px 35px rgba(174,123,169,0.5)',
+});
